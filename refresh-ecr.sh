2351bf3c0312fd8c792c6a1938ddf8ccf978eefd
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-21 22:24:02 +0530

    deploy cronjob

---
+++ refresh-ecr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e # Exit on any error
+
+# Variables expected from environment: 
+# TARGET_ROLE_ARN, REGION, SECRET_NAME, TARGET_NAMESPACE
+
+echo "Assuming IAM Role: $TARGET_ROLE_ARN"
+JSON_OUT=$(aws sts assume-role \
+  --role-arn "$TARGET_ROLE_ARN" \
+  --role-session-name "K8sRefreshSession")
+
+# Parse credentials using sed
+export AWS_ACCESS_KEY_ID=$(echo $JSON_OUT | sed -n 's/.*"AccessKeyId": "\([^"]*\)".*/\1/p')
+export AWS_SECRET_ACCESS_KEY=$(echo $JSON_OUT | sed -n 's/.*"SecretAccessKey": "\([^"]*\)".*/\1/p')
+export AWS_SESSION_TOKEN=$(echo $JSON_OUT | sed -n 's/.*"SessionToken": "\([^"]*\)".*/\1/p')
+
+echo "Fetching ECR Login Token for $REGION..."
+ECR_PASSWORD=$(aws ecr get-login-password --region "$REGION")
+
+echo "Updating Kubernetes Secret: $SECRET_NAME in $TARGET_NAMESPACE"
+kubectl create secret docker-registry "$SECRET_NAME" \
+  --docker-server="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com" \
+  --docker-username=AWS \
+  --docker-password="$ECR_PASSWORD" \
+  --namespace "$TARGET_NAMESPACE" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "ECR credentials refreshed successfully at $(date)"
